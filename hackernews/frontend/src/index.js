4803b740da9609cf40ded67bf5710ce603b36262
ybRepo <boosharya@gmail.com>
2019-01-04 17:37:41 -0800

    connecting Appolo client to backend GQL server

---
+++ hackernews/frontend/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'          //Connect the AppoloClient instance with the graphQL API server
+})
+
+const client = new ApolloClient({       //Initiate AppoloClient by passing httpLink and InMemoryCache
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(                        //The App is wrapped with the higher-order component ApolloProvider that gets passed the client as a prop.
+  <ApolloProvider client={client}>      
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
