9a07824c578913de1c5a4a88f88f0d7735c99864
ybRepo <boosharya@gmail.com>
2019-01-19 13:27:25 -0800

    order of mutations changed

--- hackernews/frontend/src/gqlQueries/mutation.js
+++ hackernews/frontend/src/gqlQueries/mutation.js
@@ -1,5 +1,31 @@
 import gql from 'graphql-tag'
 
+export const SIGNUP_MUTATION = gql `
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`
+
+export const LOGIN_MUTATION = gql `
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`
+
+export const POST_MUTATION = gql `
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      url
+      description
+    }
+  }
+`
+
 export const VOTE_MUTATION = gql `
   mutation VoteMutation($linkId: ID!) {
     vote(linkId: $linkId) {
@@ -15,32 +41,6 @@ export const VOTE_MUTATION = gql `
       user {
         id
       }
-    }
-  }
-`
-
-export const POST_MUTATION = gql `
-  mutation PostMutation($description: String!, $url: String!) {
-    post(description: $description, url: $url) {
-      id
-      url
-      description
-    }
-  }
-`
-
-export const SIGNUP_MUTATION = gql `
-  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
-    signup(email: $email, password: $password, name: $name) {
-      token
-    }
-  }
-`
-
-export const LOGIN_MUTATION = gql `
-  mutation LoginMutation($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
     }
   }
 `
